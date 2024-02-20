8672ee48098f051cf60c7aa1dbef185aa65a1ebe
mohanapriyabtech <mohana.priya@sparkouttech.com>
2024-02-21 00:37:45 +0530

    error logs added

--- src/ErrorDashboard.js
+++ src/ErrorDashboard.js
@@ -5,6 +5,7 @@ import "./ErrorDashboard.css"
 const ErrorDashboard = () => {
   const [items, setItems] = useState([]);
   const [names, setNames] = useState([]);
+  const [error, setError] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
   const [selectedName, setSelectedName] = useState('');
 
@@ -42,6 +43,16 @@ const ErrorDashboard = () => {
 
   const authToken = "bc4db8c049893fab4b577d57a38a4774:e88cc72c49cc663b6c6aac182ab1e23844e1e892bc5cd4ad9d40db54e087bf1e3445537072d7a3754616d71d0196e63e05b743b6cfef04525597094d1a826d974c86a3d988ff641e22d9a6627410e1f4b7a70a0aa4f3d891c473ac9889a3a467a5ac834a6256a4ed97243d1c2ef69cd1839b5309b9:195b2e5a3a3909613c2894e2faee2696"
 
+  const handleViewErrorAlerts = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/logs/get-error-logs');
+      setError(response.data.data);
+      setIsOpen(true);
+    } catch (error) {
+      console.error('Error fetching error logs:', error);
+    }
+  };
+
   return (
     <div className="error-dashboard">
       <h1>Error Tracking Dashboard</h1>
@@ -53,7 +64,35 @@ const ErrorDashboard = () => {
         ))}
       </select>
 
-      {selectedName && (
+      <button onClick={handleViewErrorAlerts}>View Error Alerts</button>
+      {isOpen && (
+        <div className="error-alerts">
+          <h3>Error Alerts</h3>
+          <table className="error-table">
+            <thead>
+              <tr>
+                <th>UTC TIME</th>
+                <th>LOCAL TIME</th>
+                <th>URL</th>
+                <th>Method</th>
+                <th>Status Message</th>
+              </tr>
+            </thead>
+            <tbody>
+              {error.map(item => (
+                <tr key={item.id}>
+                  <td>{item.created_at}</td>
+                  <td>{new Date(item.created_at).toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' })}</td>
+                  <td>{item.request_url}</td>
+                  <td>{item.method}</td>
+                  <td>{item.status_message}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+      {!isOpen && selectedName && (
         <div className="selected-name">
           <h2>Selected Name:</h2>
           <p>
@@ -61,28 +100,30 @@ const ErrorDashboard = () => {
           </p>
         </div>
       )}
-      <table className="error-table">
-        <thead>
-          <tr>
-            <th>UTC TIME</th>
-            <th>LOCAL TIME</th>
-            <th>URL</th>
-            <th>Method</th>
-            <th>Status Message</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map(item => (
-            <tr key={item.id}>
-              <td>{item.created_at}</td>
-              <td>{new Date(item.created_at).toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' })}</td>
-              <td>{item.request_url}</td>
-              <td>{item.method}</td>
-              <td>{item.status_message}</td>
+      {!isOpen && (
+        <table className="error-table">
+          <thead>
+            <tr>
+              <th>UTC TIME</th>
+              <th>LOCAL TIME</th>
+              <th>URL</th>
+              <th>Method</th>
+              <th>Status Message</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {items.map(item => (
+              <tr key={item.id}>
+                <td>{item.created_at}</td>
+                <td>{new Date(item.created_at).toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' })}</td>
+                <td>{item.request_url}</td>
+                <td>{item.method}</td>
+                <td>{item.status_message}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
